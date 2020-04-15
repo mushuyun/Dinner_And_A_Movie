47246cf83bee41d4ac2a49af975db4cb8b22d219
Shuyun Mu <mushuyun@yahoo.com>
2020-04-15 19:35:52 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,30 +1,23 @@
 
 module.exports = {
+    module.exports = {
     development: {
+      host:"localhost",
       username: "root",
       password: process.env.SEQUELIZE_PASSWORD,
       database: "movie_dinner",
-      details: {
-        host: "localhost",
-        port: 3306,
-        dialect: "mysql"
-      }
+      dialect: "mysql"
     },
     test: {
+      host:"localhost",
       username: "root",
       password: process.env.SEQUELIZE_PASSWORD,
       database: "movie_dinner_test",
-      details: {
-        host: "localhost",
-        port: 3306,
-        dialect: "mysql",
-        logging: false
-      }
+      dialect: "mysql",
     },
+     
     production: {
       'use_env_variable': 'JAWSDB_URL',
-      details: {
-        dialect: "mysql"
-      }
+      dialect: "mysql"
     }
   };
