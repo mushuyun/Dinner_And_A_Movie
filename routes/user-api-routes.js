e4485cec3932bc63e9e356a1b886f76544c29b14
mushuyun <mushuyun@yahoo.com>
2020-04-19 20:17:36 -0400

    fixed backend axios calls

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,12 +1,11 @@
 
-
 const request = require("request");
 var db = require("../models");
 var moment = require("moment");
 const nodemailer = require("nodemailer");
 const firebase = require("firebase");
+const axios = require("axios");
 const fbApp = require("../config/fb-config");
-var axios = require("axios");
 
 module.exports = function (app) {
   app.get("/signup", function (req, res) {
@@ -22,15 +21,13 @@ module.exports = function (app) {
 
   // POST route for new user
   app.post("/signup", function (req, res) {
-    //var database = firebase.database();
-   // var user = firebase.auth().currentUser;
+    
+    
     var email = req.body.email;
     var password = req.body.pswd;
     var displayName = req.body.displayName;
-    // var uid = req.body.uid;
-    // var uid;
-    fbApp
-      .createUserWithEmailAndPassword(email, password)
+    console.log(password);
+    fbApp.createUserWithEmailAndPassword(email, password)
       .then((data) => {
         const uid = data.uid
         db.User.create({
@@ -44,7 +41,7 @@ module.exports = function (app) {
       .catch(function (error) {
         res.statusCode = 404;
       });
-     // res.redirect("/movie-dinner");
+     
   });
 
   app.post("/api/authenticate", function (req, res) {
@@ -101,20 +98,138 @@ module.exports = function (app) {
     }
   });
 
-  app.post("movie-dinner", function (req, res) {
-  var genreId = req.body.genreId;
-  var genreName = req.body.genreName;
-  var userId = req.body.userId;
+  app.post("/add-movie-dinner", function (req, res) {
+    var genreId = req.body.genreId;
+    var genreName = req.body.genreName;
 
-      db.Movie.create({
-      genreId: parseInt(genreId),
-      genreName: genreName,
-      UserId: parseInt(userId)
-      }).then(function (res) {
-      console.log(res);
+    const user = firebase.auth().currentUser;
+    if (user) {
+      db.User.findOne({
+        where: {
+          email: user.email,
+        },
+      }).then(function(user) {
+        db.Movie.create({
+          genreId: genreId,
+          genreName: genreName,
+          UserId: user.id
+        }).then(function (res) {
+          console.log(res);
         });
+      });
+    }
+  });
+
+  app.get("/", function (req, res) {
+    res.render("login", { title: "Signin Page" });
+  });
+
+  //find all users
+  app.get("/api/users", function(req, res) {
+    
+    db.User.findAll({
+      include: [db.Movie]
+    }).then(function(dbUser) {
+      res.json(dbUser);
     });
-  
+  });
+
+  app.get("/api/movies", function(req, res) {
+    
+    db.Movie.findAll({
+      include: [db.User]
+    }).then(function(dbUser) {
+      res.json(dbUser);
+    });
+  });
+
+  //find user by Id
+  app.get("/api/users/:id", function(req, res) {
+   db.User.findOne({
+     where: {
+      id: req.params.id
+     },
+     include: [db.Movie]
+     }).then(function(dbUser) {
+      res.json(dbUser);
+     });
+  });
+
+  app.get("/api/movies/:id", function(req, res) {
+   db.Movie.findOne({
+       where: {
+         id: req.params.id
+       },
+       include: [db.User]
+     }).then(function(dbMovie) {
+       res.json(dbMovie);
+    });
+   });
+ 
+  //Need to be fix minor connection issue
+  app.get("/send", function(req, res){
+
+    res.render("send", { title: "email Page" });
+  });
+  //POST route for nodemailer
+  app.post("/send", function (req, res) {
+    
+     db.User.findOne({
+      // var recipient =req.body.email;
+
+       where: {
+         email: req.body.email,
+       },
+       include: [db.Movie]
+    }).then(function (result) {
+      console.log(result);
+      var genreId = result.genreId;
+      var today = moment().format("YYYY-MM-DD");
+       var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=" + process.env.TMDB_API_KEY + "&language=en-US&region=US&sort_by=release_date.asc&include_video=false&page=1&primary_release_date.gte=" + today + "&with_genres=" + genreId;
+
+       request(queryURL, function (error, response, body) {
+         console.log("error:", error); // Print the error if one occurred
+         console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+         var moviesBody = JSON.parse(body);   
+         var imgToEmail = " <img src='https://image.tmdb.org/t/p/w500/" + movies[i].poster_path; + "' />";     
+        console.log("Nodemailer sending to: " + result.email);
+
+         let transporter = nodemailer.createTransport({
+           service: "gmail",
+           auth: {
+             user: "yousue891@gmail.com",
+             pass: "Tha,bto7",
+           },
+           tls: {
+             rejectUnauthorized: false,
+           },
+         });
+
+         let mailOptions = {
+          from: "yousue891@gmail.com",
+          replyTo: "youseu891@gmail.com",
+           to: recipient,
+           subject: "Here are the movies you requested!",
+           text: "'" + moviesBody.results[0].title + "', Release Date: " + moviesBody.results[0].release_date
+                      };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent");
+          }
+        });
+
+        console.log("Movies successfully sent: " + moviesBody);
+
+        res.send(true);
+      });
+    });  
+  });
+  //  axios code
+
+
   app.get("/api/movie-dinner/genres", function(req, res){
     var genreId = req.body.genreId;
     var genreName = req.body.genreName;
@@ -160,134 +275,4 @@ module.exports = function (app) {
       }
     });
 
-       
-  app.get("/", function (req, res) {
-      res.render("login", { title: "Signin Page" });
-    });
-
-  //find all users
-  app.get("/api/users", function(req, res) {
-    
-    db.User.findAll({
-      include: [db.Movie]
-    }).then(function(dbUser) {
-      res.json(dbUser);
-    });
-  });
-
-  app.get("/api/movies", function(req, res) {
-    
-    db.Movie.findAll({
-      include: [db.User]
-    }).then(function(dbUser) {
-      res.json(dbUser);
-    });
-  });
-
-  //find user by Id
-  app.get("/api/users/:id", function(req, res) {
-   db.User.findOne({
-     where: {
-      id: req.params.id
-     },
-     include: [db.Movie]
-     }).then(function(dbUser) {
-      res.json(dbUser);
-     });
-  });
-// leave lines 150 -159 commented out
-
-  // app.delete("/delete/users/:id", function(req, res) {
-  //   db.User.destroy({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   }).then(function(dbUser) {
-  //     res.json(dbuser);
-  //   });
-  // });
-
-  app.get("/api/movies/:id", function(req, res) {
-   db.Movie.findOne({
-       where: {
-         id: req.params.id
-       },
-       include: [db.User]
-     }).then(function(dbMovie) {
-       res.json(dbMovie);
-    });
-   });
-// leave lines 170 -178 commented out
-  // app.delete("/delete/api/movies/:id", function(req, res) {
-  //   db.Movie.destroy({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   }).then(function(dbMovie) {
-  //     res.json(dbMovie);
-  //   });
-  // });
-
- 
-  //Need to be fix minor connection issue
-  app.get("/send", function(req, res){
-
-    res.render("send", { title: "email Page" });
-  });
-  //POST route for nodemailer
-  app.post("/send", function (req, res) {
-    
-     db.User.findOne({
-      // var recipient =req.body.email;
-
-       where: {
-         email: req.body.email,
-       },
-       include: [db.Movie]
-    }).then(function (result) {
-      console.log(result);
-      var genreId = result.genreId;
-      var today = moment().format("YYYY-MM-DD");
-       var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=TMDB_API_KEY&language=en-US&region=US&sort_by=release_date.asc&include_video=false&page=1&primary_release_date.gte=" + today + "&with_genres=" + genreId;
-
-       request(queryURL, function (error, response, body) {
-         console.log("error:", error); // Print the error if one occurred
-         console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-         var moviesBody = JSON.parse(body);   
-         var imgToEmail = " <img src='https://image.tmdb.org/t/p/w500/" + movies[i].poster_path; + "' />";     
-        console.log("Nodemailer sending to: " + result.email);
-
-         let transporter = nodemailer.createTransport({
-           service: "gmail",
-           auth: {
-             user: "yousue891@gmail.com",
-             pass: "Tha,bto7",
-           },
-           tls: {
-             rejectUnauthorized: false,
-           },
-         });
-
-         let mailOptions = {
-          from: "yousue891@gmail.com",
-          replyTo: "youseu891@gmail.com",
-           to: recipient,
-           subject: "Here are the movies you requested!",
-           text: "'" + moviesBody.results[0].title + "', Release Date: " + moviesBody.results[0].release_date
-                      };
-
-        transporter.sendMail(mailOptions, function (error, info) {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log("Email sent");
-          }
-        });
-
-        console.log("Movies successfully sent: " + moviesBody);
-
-        res.send(true);
-      });
-    });
-  }); 
 }
